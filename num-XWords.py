7eeccdba0bf2ddd7e5c1ac0d90dc9b17104d9374
SharanyaVR <sharanyavr93@gmail.com>
2014-05-26 17:33:34 +0530

    deleted:    crossword 	new file:   files/crossword - grid data - input to the whole program 	renamed:    sowpods.txt -> files/sowpods.txt 	new file:   files/testinsertionintocrossword - testdata file 	modified:   num-XWords.py - added methods for inserting a string at a particular location in crossword

--- num-XWords.py
+++ num-XWords.py
@@ -4,7 +4,7 @@ def loadGrid(gridfile):
     grid = []
     for line in open(gridfile):
         grid.append(line.strip())
-    return len(grid), ''.join(grid)
+    return len(grid), ''.join(grid), grid
 
 def get_h_triads(grid, size):
     start = 0
@@ -34,11 +34,50 @@ def assign_numbers(locations):
             numbers[location] = number
     return numbers
 
+#def backtrackLastInsertion(indexofLastWordInserted):
+#    location = getLocationofWordIndex(indexofLastWordInserted)
+     
+def insertWordHorizontal(grid, s, row, col, size):
+    newstr = list(grid[row])
+    for ch in s:
+        newstr[col] = ch
+        col += 1
+    print(newstr)
+    grid[row] = ''.join(newstr)
+    print(grid)
+    return grid
+
+def insertWordVertical(grid, s, row, col, size):
+    for ch in s:
+        newstr = list(grid[row])
+        newstr[col] = ch
+        grid[row] = ''.join(newstr)
+        row += 1
+    return grid
+
+def insertIntoCrossword(grid, s, indexofWord, locationofWord, acrossorDown, size):
+    if s != None:
+        print(s)
+        col = locationofWord[1]    
+        row = locationofWord[0]       
+        if acrossorDown == 'a':
+            grid = insertWordHorizontal(grid, s, row, col, size)
+            print("if")
+        else:
+            print("else")
+            grid = insertWordVertical(grid, s, locationofWord, size)
+    return grid
+
+
 import sys
-size, grid = loadGrid(sys.argv[1])
-h_num_locations = get_h_triads(grid, size)
-v_num_locations = get_v_triads(grid, size)
+size, gridstring, grid  = loadGrid(sys.argv[1])
+h_num_locations = get_h_triads(gridstring, size)
+v_num_locations = get_v_triads(gridstring, size)
 numbers = assign_numbers(h_num_locations + v_num_locations)
 for loc in numbers.keys():
     print(loc, numbers[loc])
+grid = insertIntoCrossword(grid, "abc", 2, (2,1), 'a',size)
+print(grid)
 
+
+ 
