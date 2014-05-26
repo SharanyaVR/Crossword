fa6f1a04181e014e15f02257521301ccd4b27f0b
mrudulaponnada <mrudulaponnada015@gmail.com>
2014-05-26 15:53:11 +0530

    test case has been added to len_word_Dict.py some invalid dadta has been added to sowpods.txt

--- len_word_Dict.py
+++ len_word_Dict.py
@@ -1,8 +1,24 @@
 words ={}
+LineNum = 0
+#test_data = ["", "123"," @#"]
+
+#test_data = ["", "123"," @#"]
+def test_case(line):
+    test_data = ["", "123"," @#"]
+    if line not in test_data:
+        return "valid"
+    else:
+        return "invalid"
+
+
 for line in open("sowpods.txt"):
     line = line.strip()
+    if test_case(line) == "invalid":
+        print("invalid")
+        print(line)
+        break
     if len(line) not in words:
         words[len(line)] = []
     words[len(line)].append(line)
 print(words)
-  
+
