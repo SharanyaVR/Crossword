3523c9f2a86d2aec96a0c2941d97024fd3ca30df
mrudulaponnada <mrudulaponnada015@gmail.com>
2014-05-27 10:35:12 +0530

    modified:   len_word_Dict.py -- test case has been added 	new file:   sampldict1 -- test input data2 	new file:   sampldict2 -- test input data3 	modified:   testdata.txt 	modified:   testdict -- main test file

--- len_word_Dict.py
+++ len_word_Dict.py
@@ -1,23 +1,24 @@
-words ={}
-def addToDict(filename):
-    for line in open(filename):
+def addToDict(wordfile, words):
+    for line in open(wordfile):
         line = line.strip()
         if len(line) not in words:
             words[len(line)] = []
         words[len(line)].append(line)
     return words
 
-def test_func(filename):
-    for test_line in open(filename):
+def test_func(testfile):
+    for test_line in open(testfile):
         sampdict, exp_output= test_line.split('!')
-        words = addToDict(sampdict)
-        print(words)
-        print(exp_output)
-        if str(words) != exp_output:
+        words = {}
+        words = addToDict(sampdict, words)
+        #print(words)
+        #print(exp_output)
+        if str(words) in exp_output:
             print("test suceeded for " + sampdict)
         else:
             print("test failed for " + sampdict)
 
 test_func("testdict")
-
+#words = {}
+#addToDict("sowpods.txt", words)
 
